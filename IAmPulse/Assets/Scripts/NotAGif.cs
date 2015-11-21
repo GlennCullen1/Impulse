38a9b26d62934caecf1a21376d9cbbdaa32e565c
Glenn Cullen <1100862@MC10791.uad.ac.uk>
2015-11-21 15:10:26 +0000

    added more UI

---
+++ IAmPulse/Assets/Scripts/NotAGif.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class NotAGif : MonoBehaviour {
+
+	[SerializeField] private Sprite[] imageReel;
+	[SerializeField] private float time;
+	private float count;
+	private int numOfImages;
+	private int activeImage;
+	// Use this for initialization
+	void Start () {
+		numOfImages = imageReel.Length;
+		count =  0;
+		activeImage = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		count += Time.deltaTime;
+		if (count >= time) {
+			activeImage++;
+			if(activeImage>numOfImages-1)
+			{
+				activeImage = 0;
+			}
+			this.GetComponent<Image>().sprite = imageReel[activeImage];
+		}
+	}
+}
